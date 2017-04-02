bcdc7670c1a413ef8902991473691f7d627bf1d4
René Winkler <rene.winkler@xovis.com>
2017-04-02 19:22:37 +0200

    transaction

--- src/main/java/ch/renewinkler/Main.java
+++ src/main/java/ch/renewinkler/Main.java
@@ -2,17 +2,26 @@ package ch.renewinkler;
 
 import ch.renewinkler.execute_around_method.FileWriterEAM;
 import ch.renewinkler.execute_around_method.Timer;
+import ch.renewinkler.execute_around_method.Transaction;
+import ch.renewinkler.execute_around_method.TransactionHandler;
 
 import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         Timer.timed(Main::dummyCalc);
 
         FileWriterEAM.use("eam.txt", writerEAM -> writerEAM.writeStuff("sweet"));
 
+        TransactionHandler.runInTransaction(connection -> {
+            //int ticketId = findAvailableTicket(connection);
+            //reserveTicket(ticketId, connection);
+            //markAsBought(ticketId, connection););
+        });
     }
+
+
 
     public static Double dummyCalc() {
         double result = 0;
